5da83c6b27767cfc014d657d5c2eb733b0ce1702
sheraz-bajwa <sheezybajwa@gmail.com>
2024-03-05 14:13:52 +0500

    first commit

---
+++ paymentstripe/lib/Homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: [
+
+      ],),
+    );
+  }
+}
